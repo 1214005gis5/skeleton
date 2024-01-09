4fc550a32b373e5cb5273236039c6db065d98ff2
Raff <rafiazhim.ra@gmail.com>
2024-01-09 18:17:08 +0700

    Initial commit

--- jscroot/url/content.js
+++ jscroot/url/content.js
@@ -30,6 +30,30 @@ export function getContentURL(){
             return url.template.content+"saved.html";
         case "logout":
             return url.template.content+"logout.html";
+        case "crud/get":
+            return url.template.content+"geojson/table.html";
+        case "map/ch1":
+            return url.template.content+"geojson/ch1/index.html";
+        case "map/ch2":
+            return url.template.content+"geojson/ch2.html";
+        case "map/ch3":
+            return url.template.content+"geojson/ch3.html";
+        case "map/ch4":
+            return url.template.content+"geojson/ch4.html";
+        case "notifications/geointersect":
+            return url.template.content + "notifications/geointersect.html";
+        case "notifications/geowithin":
+            return url.template.content + "notifications/geowithin.html";
+        case "notifications/geonear":
+            return url.template.content + "notifications/geonear.html";
+        case "notifications/geonearsphere":
+            return url.template.content + "notifications/geonearsphere.html";
+        case "notifications/geomaxdistence":
+            return url.template.content + "notifications/geomaxdistence.html";
+        case "notifications/geomindistance":
+            return url.template.content + "notifications/geomindistance.html";
+        case "notifications/geometry":
+            return url.template.content + "notifications/geometry.html";
         default:
             return url.template.content+"home.html";
     }
@@ -65,6 +89,30 @@ export function getURLContentJS(){
             return url.view.content+"saved.js";
         case "logout":
             return url.view.content+"logout.js";
+        case "crud/get":
+            return url.template.content+"geojson/table.html";
+        case "map/ch1":
+            return url.template.content+"geojson/ch1.html";
+        case "map/ch2":
+            return url.template.content+"geojson/ch2.html";
+        case "map/ch3":
+            return url.template.content+"geojson/ch3.html";
+        case "map/ch4":
+            return url.template.content+"geojson/ch4.html";
+        case "notifications/geointersect":
+            return url.template.content + "notifications/geointersect.html";
+        case "notifications/geowithin":
+            return url.template.content + "notifications/geowithin.html";
+        case "notifications/geonear":
+            return url.template.content + "notifications/geonear.html";
+        case "notifications/geonearsphere":
+            return url.template.content + "notifications/geonearsphere.html";
+        case "notifications/geomaxdistence":
+            return url.template.content + "notifications/geomaxdistence.html";
+        case "notifications/geomindistance":
+            return url.template.content + "notifications/geomindistance.html";
+        case "notifications/geometry":
+            return url.template.content + "notifications/geometry.html";
         default:
             return url.view.content+"home.js";
     }
